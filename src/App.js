1d28f7b87d5ccd8f507e1da3fb2fe8cf3b7b9ce6
WilliamJGrace <williamjgrace@outlook.com>
2020-04-27 17:48:40 +0100

    code broken

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import hash from "./hash";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+const clientId = "a1510bcde0e44f96bc83ea4479786a45";
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+
+const hashurl = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+
+  window.location.hash = "";
+
+  class App extends Component {
+    componentDidMount() {
+      // Set token
+      let _token = hashurl.access_token;
+      if (_token) {
+        // Set token
+        this.setState({
+          token: _token
+        });
+      }
+      render() {
+        return (
+          <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <img src={logo} className="App-logo" alt="logo" />
+      {!this.state.token && (
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
         >
-          Learn React
+          Login to Spotify
         </a>
+      )}
+      {this.state.token && (
+        // Spotify Player Will Go Here In the Next Step
+      )}
       </header>
     </div>
   );
+  }
+}
 }
 
 export default App;
