220355763a4ffec8273c38c7bdfb284809f0dae7
WilliamJGrace <williamjgrace@outlook.com>
2020-04-27 18:40:38 +0100

    it actually works - doesnt work in real time

--- src/App.js
+++ src/App.js
@@ -1,61 +1,87 @@
 import React, { Component } from "react";
+import * as $ from "jquery";
+import { authEndpoint, clientId, redirectUri, scopes } from "./config_example";
 import hash from "./hash";
+import Player from "./Player";
 import logo from "./logo.svg";
 import "./App.css";
 
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+      item: {
+        album: {
+          images: [{ url: "" }]
+        },
+        name: "",
+        artists: [{ name: "" }],
+        duration_ms: 0
+      },
+      is_playing: "Paused",
+      progress_ms: 0
+    };
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+  }
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
 
-const clientId = "a1510bcde0e44f96bc83ea4479786a45";
-const redirectUri = "http://localhost:3000";
-const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-];
-
-const hashurl = window.location.hash
-  .substring(1)
-  .split("&")
-  .reduce(function(initial, item) {
-    if (item) {
-      var parts = item.split("=");
-      initial[parts[0]] = decodeURIComponent(parts[1]);
-    }
-    return initial;
-  }, {});
-
-  window.location.hash = "";
-
-  class App extends Component {
-    componentDidMount() {
+    if (_token) {
       // Set token
-      let _token = hashurl.access_token;
-      if (_token) {
-        // Set token
+      this.setState({
+        token: _token
+      });
+      this.getCurrentlyPlaying(_token);
+    }
+  }
+
+  getCurrentlyPlaying(token) {
+    // Make a call using the token
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: xhr => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: data => {
         this.setState({
-          token: _token
+          item: data.item,
+          is_playing: data.is_playing,
+          progress_ms: data.progress_ms
         });
       }
-      render() {
-        return (
-          <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      {!this.state.token && (
-        <a
-          className="btn btn--loginApp-link"
-          href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
-        >
-          Login to Spotify
-        </a>
-      )}
-      {this.state.token && (
-        // Spotify Player Will Go Here In the Next Step
-      )}
-      </header>
-    </div>
-  );
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {!this.state.token && (
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                "%20"
+              )}&response_type=token&show_dialog=true`}
+            >
+              Login to Spotify
+            </a>
+          )}
+          {this.state.token && (
+            <Player
+              item={this.state.item}
+              is_playing={this.state.is_playing}
+              progress_ms={this.progress_ms}
+            />
+          )}
+        </header>
+      </div>
+    );
   }
 }
-}
+
 
 export default App;
---
+++ src/config_example.js
@@ -0,0 +1,9 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+export const clientId = "a1510bcde0e44f96bc83ea4479786a45";
+export const redirectUri = "http://localhost:3000";
+export const scopes = [
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
